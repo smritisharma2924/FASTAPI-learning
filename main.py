f20992b6cae7fcbf174c66e98556536a71ba144f
smritisharma2924 <smritisharma2905@gmail.com>
2026-06-13 15:31:26 +0530

    Created few API endpoints

--- main.py
+++ main.py
@@ -20,3 +20,11 @@ def about() :
 def view() :
     data = load_data()
     return data
+
+@app.get("/patient/{patient_id}")
+def view_patient(patient_id : str) :
+    data = load_data()
+    if patient_id in data :
+        return data[patient_id]
+    else :
+        return {"Error" : "Patient not found."}
